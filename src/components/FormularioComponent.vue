b3269d7b52ea5ad38103d18675d765835b49d2cf
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-10-03 22:56:26 -0300

    Vuex implementation

--- src/components/FormularioComponent.vue
+++ src/components/FormularioComponent.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="box formulario">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulario para a criação de uma nova tarefa">
+            <div class="column is-5" role="form" aria-label="Formulario para a criação de uma nova tarefa">
                 <input 
                 type="text" 
                 class="input" 
                 placeholder="Qual a tarefa você deseja iniciar?"
                 v-model="descricaoTarefa">
+            </div>
+
+            <div class="column is-3">
+                <div class="select">
+                <select v-model="idProjeto">
+                    <option value="">Selecione o projeto</option>
+                    <option
+                    :value="projeto.id"
+                    v-for="projeto in projetos"
+                    :key="projeto.id"
+                    >
+                    {{ projeto.nome }}
+                    </option>
+                </select>
+                </div>
             </div>
 
             <div class="column">
@@ -28,6 +43,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TemporizadorComponent from '@/components/TemporizadorComponent.vue'
+import { useStore } from 'vuex';
+import { key } from "@/store"
+import IProjeto from '@/interfaces/IProjeto';
 
 export default defineComponent({
     name: "FormularioComponent",
@@ -35,16 +53,24 @@ export default defineComponent({
     components: {
         TemporizadorComponent
     },
+    setup(){
+        const store = useStore(key)
+        return {
+            projetos: store.state.projetos
+        }
+    },
     data(){
         return {
-            descricaoTarefa: ''
+            descricaoTarefa: '',
+            idProjeto: ''
         }
     },
     methods: {
         finalizarTarefa(tempoDecorrido: number) : void{
             this.$emit('aoSalvarTarefa', {
                 duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricaoTarefa
+                descricao: this.descricaoTarefa,
+                projeto: this.projetos.find( (proj: IProjeto) => proj.id === this.idProjeto)
             })
             this.descricaoTarefa = ''
         }
